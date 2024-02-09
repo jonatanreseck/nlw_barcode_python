1ecd23e9cdf700f3698a4af8203b5d9e649c436a
jonatanreseck <jonatanreseck@yahoo.com.br>
2024-02-08 23:51:10 -0300

    feat: controller of tag

--- nlw/src/views/tag_creator_view.py
+++ nlw/src/views/tag_creator_view.py
@@ -1,13 +1,15 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreateController
 
 class TagCreatorView:
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_create_controller = TagCreateController()
         body = http_request.body
         product_code = body["product_code"]
 
         # lógica da regra de negócio
-        print('estou na view')
+        formatted_response = tag_create_controller.create(product_code)
         # retorno http
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
